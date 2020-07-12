e87bd29b8ec94408bde1d9e7130b673ad17f8b1e
Naoto Tanaka <7010raimi@gmail.com>
2020-07-12 23:16:41 +0900

    TODO no

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.headerTitle}>Todoリスト</Text>
+            <TextInput style={styles.todoInput} />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>追加する</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    headerTitle: {
+        textAlign: 'center',
+        fontSize: 30,
+    },
+    todoInput: {
+        backgroundColor: '#fff',
+        width: '70%',
+        alignSelf: 'center',
+        marginTop: 20,
+        padding: 10,
+        fontSize: 20,
+    },
+    container: {
+        backgroundColor: '#ddd',
+        flex: 1,
+    },
+    button: {
+        backgroundColor: 'blue',
+        width: '60%',
+        alignSelf: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        padding: 10,
+    },
+})
