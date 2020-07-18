24306f13b1a627c7f3bb687c25680fa912f606d8
Naoto Tanaka <7010raimi@gmail.com>
2020-07-18 17:47:09 +0900

    FlatlistとSafeareaviewを導入

--- App.js
+++ App.js
@@ -4,25 +4,22 @@ import {
     StyleSheet,
     Text,
     View,
-    SafeAreaView,
-    TextInput,
-    TouchableOpacity,
+    FlatList,
+    SafeAreaView
 } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import ListItem from './components/ListItem'
+import todos from './dummies/todos.json'
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <View style={styles.itemContainer}>
-                <View style={styles.leftContainer}>
-                    <MaterialCommunityIcons name="checkbox-blank-outline" size={42} color="black" />
-                </View>
-                <TextInput style={styles.todoInput}/>
-                <View style={styles.rightContainer}>
-                    <MaterialCommunityIcons name="delete-circle-outline" size={42} color="black" />
-                </View>
-            </View>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={todos}
+                renderItem={({ item }) => (<ListItem defaultValue={item.defaultValue} />)}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </SafeAreaView>
     )
 }
 
@@ -30,34 +27,5 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#ddd',
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    itemContainer: {
-        height: 60,
-        width: "100%",
-        borderColor: "gray",
-        borderWidth: 1,
-        flexDirection: "row" 
-    },
-    leftContainer: {
-        backgroundColor: "#fff",
-        width: 60,
-        alignItems: 'center',
-        justifyContent: 'center' 
-    },
-    centerContainer: {
-        flex: 1
-    },
-    rightContainer: {
-        backgroundColor: "#fff",
-        width: 60,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    todoInput: {
-        flex: 1,
-        backgroundColor: "#fff",
-        fontSize: 20
     }
 });
