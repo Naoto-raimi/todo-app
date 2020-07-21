538cfb0424810064d28968887c3444ba75ee154e
Naoto Tanaka <7010raimi@gmail.com>
2020-07-22 00:13:52 +0900

    Homescreen及びNavigator作成

--- App.js
+++ App.js
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Layout from "./components/Layout";
-import TodoList from "./components/TodoList";
-import AddTodoForm from "./components/AddTodoForm";
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator'
 
-import { useInputValue, useTodos } from "./hooks/todolist";
+const Stack = createStackNavigator();
 
 export default function App() {
-    const { inputValue, changeInput, clearInput } = useInputValue();
-    const { todos, addTodo, checkTodo, removeTodo } = useTodos();
-
-    const clearInputAndAddTodo = () => {
-        clearInput();
-        addTodo(inputValue);
-    };
-
-      return (
-        <Layout>
-          <AddTodoForm
-            inputValue={inputValue}
-            changeInput={changeInput}
-            onIconPress={clearInputAndAddTodo}
-          />
-          <TodoList
-            items={todos}
-            onItemCheck={idx => checkTodo(idx)}
-            onItemRemove={idx => removeTodo(idx)}
-          />
-        </Layout>
+    return (
+      <AppNavigator />
     )
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import Layout from "../components/Layout";
+import TodoList from "../components/TodoList";
+import AddTodoForm from "../components/AddTodoForm";
+
+import { useInputValue, useTodos } from "../hooks/todolist";
+
+export default HomeScreen = () => {
+    const { inputValue, changeInput, clearInput } = useInputValue();
+    const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+
+    const clearInputAndAddTodo = () => {
+        clearInput();
+        addTodo(inputValue);
+    };
+
+      return (
+        <Layout>
+          <AddTodoForm
+            inputValue={inputValue}
+            changeInput={changeInput}
+            onIconPress={clearInputAndAddTodo}
+          />
+          <TodoList
+            items={todos}
+            onItemCheck={idx => checkTodo(idx)}
+            onItemRemove={idx => removeTodo(idx)}
+          />
+        </Layout>
+    )
+}
